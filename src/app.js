e3f7921155b9eba72ed2a910c500ffcd984e8783
MiliBrime <milibrime@hotmail.com>
2023-11-26 21:23:09 -0300

    carpetas nuevas y html carrito

--- src/app.js
+++ src/app.js
@@ -3,10 +3,18 @@ app=express();
 
 const path = require("path");
 
+app.use(express.static("public"));
+app.use(express.static("views"));
+
+
 app.listen(3010,()=> {
     console.log("servidor corriendo en http://localhost:3010");
 })
 
 app.get("/login", (req,res)=> {
     res.sendFile(path.resolve(__dirname, "./views/login.html"));
+})
+
+app.get("/carrito", (req,res)=> {
+    res.sendFile(path.resolve(__dirname, "./views/carrito.html"));
 })
