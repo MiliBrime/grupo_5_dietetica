a1ad68bb64068e9edc2647a6608f3120639ed914
vellidoJeremias <alumnovellido@gmail.com>
2023-11-28 22:17:23 -0300

    forms7

--- src/app.js
+++ src/app.js
@@ -25,4 +25,6 @@ app.get("/carrito", (req,res)=> {
 app.get("/register", (req,res)=> {
     res.sendFile(path.resolve(__dirname, "./views/register.html"));
 })
-
+app.post("/register", (req,res)=> {
+    res.sendFile(path.resolve(__dirname, "./home.html"));
+})
