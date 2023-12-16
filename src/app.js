8adbcd87f4e5bc7f8b9b47551012bf7537f8e10f
Tomas Montana <montanatomas4@gmail.com>
2023-12-16 17:58:45 -0300

    Revert "agregue los partials footer"
    This reverts commit 42ec3935904dab6443591bd7c3cf032415302599.

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.listen(3010,()=> {
     console.log("servidor corriendo en http://localhost:3010");
 })
 
-/* app.get("/", (req,res)=> {
+app.get("/", (req,res)=> {
     res.sendFile(path.resolve(__dirname, "./views/home.html"));
 })
 
@@ -30,31 +30,10 @@ app.get("/register", (req,res)=> {
 })
 app.get("/loginsignup", (req,res)=> {
     res.sendFile(path.resolve(__dirname, "./views/loginsignup.html"));
-}) */
-app.get("/", (req,res)=> {
-    res.render("home")
 })
-
-app.get("/login", (req,res)=> {
-    res.render("login")
-})
-
-app.get("/carrito", (req,res)=> {
-    res.render("carrito")
-})
-
-app.get("/register", (req,res)=> {
-    res.render("register")
-})
-
-app.get("/loginsignup", (req,res)=> {
-    res.render("loginsignup")
-})
-
 app.get("/detalleproducto", (req,res)=> {
     res.render("productDetail")
 })
-
 app.post("/register", (req,res)=> {
     res.sendFile(path.resolve(__dirname, "./views/home.html"));
 })
