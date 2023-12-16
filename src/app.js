93c9fa701e17097d8a3cbc493a39ac35789d1163
jerebondar <gere.bond.ar@gmail.com>
2023-12-16 18:08:47 -0300

    carrito renderizado

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.get("/login", (req,res)=> {
 })
 
 app.get("/carrito", (req,res)=> {
-    res.sendFile(path.resolve(__dirname, "./views/carrito.html"));
+    res.render("carrito")
 })
 app.get("/register", (req,res)=> {
     res.sendFile(path.resolve(__dirname, "./views/register.html"));
