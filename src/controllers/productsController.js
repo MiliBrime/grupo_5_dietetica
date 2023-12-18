baaec6a5ae3a07a4337f7339644f55e13e071125
Tomas Montana <montanatomas4@gmail.com>
2023-12-18 20:43:19 -0300

    "termine" los routes que estaba haciendo
    obviamente podemos borrar algunos e ir haciendolos de a poco

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -4,11 +4,41 @@ const path = require('path');
 const productsFilePath = path.join(__dirname, '../data/products.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
+const controller = {
+	// (get) Root - Mostrar todos los productos
+	index: (req, res) => {
+		// Do the magic
+	},
 
+	// (get) Detail - Detalle de un producto
+	detail: (req, res) => {
+		// Do the magic
+	},
 
+	// (get) Create - Formulario para crear
+	create: (req, res) => {
+		// Do the magic
+	},
+	
+	// (post) Create - Método para guardar la info
+	store: (req, res) => {
+		// Do the magic
+	},
 
+	// (get) Update - Formulario para editar
+	edit: (req, res) => {
+		// Do the magic
+	},
+	// (post) Update - Método para actualizar la info
+	update: (req, res) => {
+		// Do the magic
+	},
 
-
+	// (delete) Delete - Eliminar un producto de la DB
+	destroy : (req, res) => {
+		// Do the magic
+	}
+};
 
 
 
--- src/routes/products.js
+++ src/routes/products.js
@@ -5,13 +5,28 @@ const router = express.Router();
 // requires del controlador
 const productsController = require('../controllers/productsController');
 
+// Devolver todos los productos  
+router.get('/', productsController.index);
+
+// Devolver un producto 
+router.get('/detail/:id/', productsController.detail); 
+
+/* Quiten el comentario y editen según el enunciado
 
 
+// Crear un producto
+router.???('/???/', productsController.create); 
+router.???('/', productsController.store); 
 
 
+// Editar un producto 
+router.???('/:id/???', productsController.edit); 
+router.???('/:id', productsController.update); 
 
+// Eliminar un producto 
+router.???('/:id', productsController.destroy);
 
-
+*/
 
 
 module.exports = router;
