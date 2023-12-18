755f9965a6a508b6f6d636430f3ba907bc18ef3a
Tomas Montana <montanatomas4@gmail.com>
2023-12-18 20:32:04 -0300

    agregue ruta del controlador del pruducto

---
+++ src/controllers/productsController.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/products.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+
+
+
+
+
+
+
+
+module.exports = controller;
---
+++ src/routes/products.js
@@ -0,0 +1,17 @@
+// Requires
+const express = require('express');
+const router = express.Router();
+
+// requires del controlador
+const productsController = require('../controllers/productsController');
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
