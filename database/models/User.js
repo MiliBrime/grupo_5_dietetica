238bc5716ee6f03d3a2ca8a53b01e3cdca702712
MiliBrime <milibrime@hotmail.com>
2024-02-16 18:59:17 -0300

    modelo Users

---
+++ database/models/User.js
@@ -0,0 +1,36 @@
+module.exports= (sequelize, dataTypes) => {
+    let alias = "User";
+    let columns = {
+        id:{
+            autoincrement: true,
+            primaryKey: true,
+            type: dataTypes.INTEGER
+        },
+        first_name: {
+            allowNull: false,
+            type: dataTypes.STRING
+        },
+        last_name: {
+            allowNull: false,
+            type: dataTypes.STRING
+        },
+        phone:{
+            type: dataTypes.STRING
+        },
+        email:{
+            allowNull: false,
+            type: dataTypes.STRING
+        },
+        password:{
+            allowNull: false,
+            type: dataTypes.STRING
+        },
+        photo:{
+            type: dataTypes.STRING
+        }
+    };
+    let config = { tableName: "Users", timestamps: false };
+    
+    const User= sequelize.define (alias,columns,config)
+	return User; 
+}
