a8cd61bed0aee3905a54bfb017ca8c844d5a0ba2
MiliBrime <milibrime@hotmail.com>
2024-02-25 18:29:46 -0300

    barra desplegable de categorias

---
+++ public/js/categories.js
@@ -0,0 +1,17 @@
+function toggleDropdown() {
+    let dropdown= document.querySelector("#myDropdown")
+dropdown.classList.toggle("show");
+  }
+  
+  // Cierra el menú si el usuario hace clic fuera de él
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) { //si algo q toca no coincide con dropbtn que es categorias
+      let contenidoDropdown = document.getElementsByClassName("dropdown-content");
+      for (let i = 0; i < contenidoDropdown.length; i++) {
+        let openDropdown = contenidoDropdown[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
