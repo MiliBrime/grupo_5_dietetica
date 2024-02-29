0646debf44761f2542514ec98e7f14831b22d675
MiliBrime <milibrime@hotmail.com>
2024-02-29 15:28:04 -0300

    arreglo en profile que no permitia que se vea bien la imagen por defecto

--- src/middlewares/userLogged.js
+++ src/middlewares/userLogged.js
@@ -1,46 +1,26 @@
-let userModel= require ("../models/users");
-
 async function  userLoggedMiddleware(req,res,next){ 
     res.locals.isLogged = false;
+/*      res.locals.userIsAdmin = false;
+ */    
+    let cookieUser = req.cookies.userEmail; //userEmail es el nombre de la cookie
     
-    let emailInCookie = req.cookies.userEmail; //userEmail es el nombre de la cookie
-    
-    if(emailInCookie) { 
-        try{
-            let userFromCookie = await userModel.findByField("email", emailInCookie);
-            
-            if(userFromCookie){
-                //si alguien puso para recordarlo, hay un usuario en la cookie, y ese va a ser el user logueado
-                req.session.userLogged = userFromCookie;
-            }
-        } catch(error){
-            console.error("error devolviendo usuario de la db: ", error)
-        }
+     /* if(req.session.admin){
+        res.locals.userIsAdmin = true;
+        res.locals.admin = req.session.admin;
+    }  */
+
+    if(cookieUser) { 
+        req.session.userLogged = cookieUser;
     }
     
     if (req.session && req.session.userLogged){
         res.locals.isLogged = true;
+        delete res.locals.userLogged;
         res.locals.userLogged= req.session.userLogged //paso lo q tengo en sesion a una variable local, para poder usar el nombre del usuario en la nav var
     }
 
     next();}
     
 module.exports = userLoggedMiddleware;
-  
-/* function userLoggedMiddleware(req, res, next) {
-    res.locals.isLogged = false;
 
-    let emailInCookie = req.cookies.userEmail; //aca queda el mail del q ingresó
 
-    if (emailInCookie) { //si puso recordar
-        res.locals.isLogged = true;
-        res.locals.userLogged = emailInCookie;
-
-        aca faltaria poner toda la info del user logueado en req.session y tambien falta
-        res.locals.userLogged= req.session.userLogged //paso lo q tengo en sesion a una variable local, para poder usar el nombre del usuario en la nav var
-    }
-
-    next();
-}
-
-module.exports = userLoggedMiddleware; */
