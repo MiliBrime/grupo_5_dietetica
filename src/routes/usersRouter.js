2d79e6e09e250d654bbaef45d1517533f5fd847a
jerebondar <gere.bond.ar@gmail.com>
2024-03-26 22:01:58 -0300

    lo mismo con usuarios

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -19,7 +19,7 @@ const storage= multer.diskStorage({
         cb(null, 'public/img/users')
     },
     filename: function (req,file,cb){
-        cb(null, file.fieldname + " - " + Date.now() + path.extname(file.originalname)) 
+        cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname)) 
     }
 })
 
